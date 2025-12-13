dad51c2345365fcd07f9ca575a998b08bedc8146
Jafewe <mwelle231@gmail.com>
2025-12-13 13:41:53 +0100

    day 2 part 1 done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+result = 0
 f = open("input.txt")
 fileContent = f.read()
 f.close()
@@ -7,5 +8,12 @@ for ids in idRanges:
   id1 = int(ids.split('-')[0])
   id2 = int(ids.split('-')[1])
   while(id1 <= id2):
+    idString = str(id1)
+    if(len(idString) % 2 == 0):
+      splitingPosition = int(len(idString) / 2)
+      part1 = int(idString[:splitingPosition])
+      part2 = int(idString[splitingPosition:])
+      if part1 == part2:
+        result += id1
     id1 += 1
-    print(id1)
+print(result)
